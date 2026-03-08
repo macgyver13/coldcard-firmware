0cfcfec63e947ace7027a69c49346096d8010bc4
macgyver13 <4712150+macgyver13@users.noreply.github.com>
2026-03-08 12:34:33 -0400

    fix: (psbt v2) structural changes
    Only store key_data for short_values - remove key_type
    Only serialize PSBT_OUT_SCRIPT if self.script has a value
    Adjust v2 script assert to not required if sp_v0_info is present

--- shared/psbt.py
+++ shared/psbt.py
@@ -295,7 +295,8 @@ class psbtProxy:
             # storing offset and length only! Mostly.
             if kt in self.short_values:
                 actual = fd.read(vs)
-                self.store(kt, bytes(key), actual)
+                # only store key data for short_values
+                self.store(kt, bytes(key[1:]), actual)
             else:
                 # skip actual data for now
                 # TODO: could this be stored more compactly?
@@ -553,7 +554,8 @@ class psbtOutputProxy(psbtProxy):
                 wr(PSBT_OUT_MUSIG2_PARTICIPANT_PUBKEYS, v, k)
 
         if is_v2:
-            wr(PSBT_OUT_SCRIPT, self.script)
+            if self.script is not None:
+                wr(PSBT_OUT_SCRIPT, self.script)
             wr(PSBT_OUT_AMOUNT, self.amount)
 
         if self.proprietary:
@@ -1945,7 +1947,11 @@ class psbtObject(psbtProxy):
             if self.is_v2:
                 # v2 requires inclusion
                 assert o.amount is not None
-                assert o.script
+                # Silent Payments: if not spending to silent payment then script must be provided
+                if not o.sp_v0_info:
+                    assert o.script, "v2 script required when not silent payment output"
+                if o.amount == 0 and o.script == b'\x6a':
+                    null_data_op_return = True
             else:
                 # v0 requires exclusion
                 assert o.amount is None
