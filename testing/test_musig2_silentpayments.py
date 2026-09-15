Coldcard/firmware

feat (MuSig2 + SP): partial ECDH share contribution and signing UX
auth:
- Signing UX: prompt the user to contribute shares, run round 1 nonce generation.

psbt:
- Add musig_keyagg_context: assemble the keyagg cache and derive the BIP-327
  combine factors (MusigEcdhFactors).
- Allow musig sp inputs round 1 sign_it despite incomplete SP coverage.

serializations:
- determine_my_change - get_address returns (None, None) when scriptPubKey is None,  
  no longer errors on musig sp inputs before output scripts are computed.

silentpayments:
- Contribute partial shares and DLEQ proof bound to the account-level participant 
 key (_contribute_musig_ecdh_shares).
- Aggregate shares via KeyAgg coefficients and the input's combine factors (_musig_aggregate_shares, _musig_keyagg_coefficient, _musig_sp_ecdh_factors, _musig_input_ecdh_share).

Add bip327 tagged hashes

Add musig2 silent payments round 1 test and fixtures.

testing/test_musig2_silentpayments.py
@@ -1,0 +1,136 @@
+# (c) Copyright 2026 by Coinkite Inc. This file is covered by license found in COPYING-CC.
+#
+# test_musig2_silentpayments.py - Coldcard simulator tests for MuSig2 + Silent Payments (BIP-327 + BIP-352).
+#
+# Fixture PSBTs are generated by the Rust binary in bip375-examples:
+#   cargo run -p musig2-signer --bin payroll <path-to-testing/data>
+
+
+import json
+import os
+import pytest
+import time
+from psbt import BasicPSBT
+
+DATA_DIR = os.path.join(os.path.dirname(__file__), 'data')
+ROUND1_PSBT = os.path.join(DATA_DIR, 'musig2-sp-round1-in.psbt')
+# The tr(musig(A,B,C)) treasury descriptor matching the fixtures. Alice (xfp 0f056943) is the
+# simulator's default seed; the device enrolls this wallet before signing.
+TREASURY_DESC = os.path.join(DATA_DIR, 'desc-musig-sp-demo.txt')
+
+NUM_RECIPIENTS = 5
+
+SIMULATOR_DELAY = .1  # seconds to wait for the simulator to process the PSBT and update the story
+
+
+def _load_psbt(path):
+    with open(path, 'rb') as f:
+        return f.read()
+
+def _get_scan_keys(tp):
+    return {bytes(out.sp_v0_info[:33]) for out in tp.outputs if out.sp_v0_info}
+
+def _enroll_treasury(clear_miniscript, offer_minsc_import, press_select):
+    """Register the tr(musig(A,B,C)) treasury so signing runs the enrolled path."""
+    clear_miniscript()
+    with open(TREASURY_DESC) as f:
+        desc = f.read().strip()
+    _, story = offer_minsc_import(json.dumps(dict(name='sp_treasury', desc=desc)))
+    assert 'Create new miniscript wallet?' in story
+    press_select()
+
+def _our_participant_key(tp):
+    """
+    Return the participant pubkey (33 bytes) that the simulator contributed shares for.
+    We identify it by finding any entry in musig_partial_ecdh_shares on input[0].
+    Raises if none found (round 1 contribution not present).
+    """
+    inp = tp.inputs[0]
+    if not inp.musig_partial_ecdh_shares:
+        raise AssertionError("No MuSig2 partial ECDH shares found on input[0] after round 1")
+    # Composite key is (scan_key, participant_pk); the simulator only contributes its own
+    # participant key, so all entries share the same participant_pk in the second slot.
+    _, participant_key = next(iter(inp.musig_partial_ecdh_shares))
+    return participant_key
+
+# ---------------------------------------------------------------------------
+# Round 1 test
+# ---------------------------------------------------------------------------
+
+def test_musig2_sp_round1(dev, start_sign, end_sign, cap_story,
+                          clear_miniscript, offer_minsc_import, press_select):
+    """
+    Round 1: load the pre-contribution PSBT, approve the 'CONTRIBUTE SHARES?' story,
+    and confirm the simulator contributes ECDH shares (0x21), DLEQ proofs (0x22), and a
+    MuSig2 pubnonce (0x1b) for every SP scan key — without adding a signature.
+    """
+    if not os.path.exists(ROUND1_PSBT):
+        pytest.skip(f"Fixture not found: {ROUND1_PSBT}")
+
+    _enroll_treasury(clear_miniscript, offer_minsc_import, press_select)
+
+    start_sign(_load_psbt(ROUND1_PSBT))
+
+    time.sleep(SIMULATOR_DELAY)
+    title, story = cap_story()
+    if title != 'CONTRIBUTE SHARES?':
+        print("\nSTORY BODY:\n", story)
+    assert title == 'CONTRIBUTE SHARES?', f"Unexpected story title: {title!r}"
+    assert 'Silent payment ECDH shares will be added to this transaction' in story, "SP addresses should appear in round 1 story"
+
+    signed = end_sign(accept=True, finalize=False, accept_ms_import=True)
+    tp = BasicPSBT().parse(signed)
+
+    scan_keys = _get_scan_keys(tp)
+    assert len(scan_keys) == NUM_RECIPIENTS, \
+        f"Expected {NUM_RECIPIENTS} SP recipients, got {len(scan_keys)}"
+
+    _assert_round1_output(tp, scan_keys)
+
+def _assert_round1_output(tp, scan_keys):
+    """
+    After Round 1 the PSBT must have, on input[0]:
+    - One musig_partial_ecdh_shares entry per scan key, keyed by (scan_key, our_pk)
+    - One musig_partial_dleq_proofs entry per scan key, keyed by (scan_key, our_pk)
+    - One musig_pubnonces entry (our 0x1b contribution)
+    - No taproot_key_sig (coordinator hasn't aggregated yet)
+    """
+    inp = tp.inputs[0]
+
+    assert inp.musig_partial_ecdh_shares, "Round 1: 0x21 ECDH shares missing on input[0]"
+    assert inp.musig_partial_dleq_proofs, "Round 1: 0x22 DLEQ proofs missing on input[0]"
+    assert inp.musig_pubnonces,   "Round 1: 0x1b pubnonce missing on input[0]"
+    assert not inp.taproot_key_sig, "Round 1: tap_key_sig must not be present"
+
+    participant_key = _our_participant_key(tp)
+
+    # One entry per scan key in the ECDH share map
+    contributed_scan_keys_ecdh = {sk for (sk, pk) in inp.musig_partial_ecdh_shares if pk == participant_key}
+    assert contributed_scan_keys_ecdh == scan_keys, (
+        f"Round 1 ECDH share scan keys mismatch: "
+        f"got {len(contributed_scan_keys_ecdh)}, expected {len(scan_keys)}"
+    )
+
+    # One entry per scan key in the DLEQ proof map
+    contributed_scan_keys_dleq = {sk for (sk, pk) in inp.musig_partial_dleq_proofs if pk == participant_key}
+    assert contributed_scan_keys_dleq == scan_keys, (
+        f"Round 1 DLEQ proof scan keys mismatch: "
+        f"got {len(contributed_scan_keys_dleq)}, expected {len(scan_keys)}"
+    )
+
+    # ECDH share format: compressed point
+    for (sk, pk), share in inp.musig_partial_ecdh_shares.items():
+        assert len(share) == 33 and share[0] in (0x02, 0x03), \
+            f"Invalid ECDH share format for scan_key={sk.hex()}: {share.hex()}"
+
+    # DLEQ proof format: 64 bytes
+    for (sk, pk), proof in inp.musig_partial_dleq_proofs.items():
+        assert len(proof) == 64, \
+            f"DLEQ proof must be 64 bytes for scan_key={sk.hex()}, got {len(proof)}"
+
+    # Exactly one pubnonce from our participant key
+    our_pubnonces = [(pk, ak, lh) for (pk, ak, lh) in inp.musig_pubnonces if pk == participant_key]
+    assert len(our_pubnonces) == 1, \
+        f"Expected 1 pubnonce from our participant key, got {len(our_pubnonces)}"
+    pubnonce = inp.musig_pubnonces[our_pubnonces[0]]
+    assert len(pubnonce) == 66, f"Pubnonce must be 66 bytes, got {len(pubnonce)}"

macgyver13/coldcard-firmware

testing/test_musig2_silentpayments.py
@@ -1,0 +1,136 @@
+# (c) Copyright 2026 by Coinkite Inc. This file is covered by license found in COPYING-CC.
+#
+# test_musig2_silentpayments.py - Coldcard simulator tests for MuSig2 + Silent Payments (BIP-327 + BIP-352).
+#
+# Fixture PSBTs are generated by the Rust binary in bip375-examples:
+#   cargo run -p musig2-signer --bin payroll <path-to-testing/data>
+
+
+import json
+import os
+import pytest
+import time
+from psbt import BasicPSBT
+
+DATA_DIR = os.path.join(os.path.dirname(__file__), 'data')
+ROUND1_PSBT = os.path.join(DATA_DIR, 'musig2-sp-round1-in.psbt')
+# The tr(musig(A,B,C)) treasury descriptor matching the fixtures. Alice (xfp 0f056943) is the
+# simulator's default seed; the device enrolls this wallet before signing.
+TREASURY_DESC = os.path.join(DATA_DIR, 'desc-musig-sp-demo.txt')
+
+NUM_RECIPIENTS = 5
+
+SIMULATOR_DELAY = .1  # seconds to wait for the simulator to process the PSBT and update the story
+
+
+def _load_psbt(path):
+    with open(path, 'rb') as f:
+        return f.read()
+
+def _get_scan_keys(tp):
+    return {bytes(out.sp_v0_info[:33]) for out in tp.outputs if out.sp_v0_info}
+
+def _enroll_treasury(clear_miniscript, offer_minsc_import, press_select):
+    """Register the tr(musig(A,B,C)) treasury so signing runs the enrolled path."""
+    clear_miniscript()
+    with open(TREASURY_DESC) as f:
+        desc = f.read().strip()
+    _, story = offer_minsc_import(json.dumps(dict(name='sp_treasury', desc=desc)))
+    assert 'Create new miniscript wallet?' in story
+    press_select()
+
+def _our_participant_key(tp):
+    """
+    Return the participant pubkey (33 bytes) that the simulator contributed shares for.
+    We identify it by finding any entry in sp_partial_ecdh_shares on input[0].
+    Raises if none found (round 1 contribution not present).
+    """
+    inp = tp.inputs[0]
+    if not inp.sp_partial_ecdh_shares:
+        raise AssertionError("No MuSig2 partial ECDH shares found on input[0] after round 1")
+    # Composite key is (scan_key, participant_pk); the simulator only contributes its own
+    # participant key, so all entries share the same participant_pk in the second slot.
+    _, participant_key = next(iter(inp.sp_partial_ecdh_shares))
+    return participant_key
+
+# ---------------------------------------------------------------------------
+# Round 1 test
+# ---------------------------------------------------------------------------
+
+def test_musig2_sp_round1(dev, start_sign, end_sign, cap_story,
+                          clear_miniscript, offer_minsc_import, press_select):
+    """
+    Round 1: load the pre-contribution PSBT, approve the 'CONTRIBUTE SHARES?' story,
+    and confirm the simulator contributes ECDH shares (0x21), DLEQ proofs (0x22), and a
+    MuSig2 pubnonce (0x1b) for every SP scan key — without adding a signature.
+    """
+    if not os.path.exists(ROUND1_PSBT):
+        pytest.skip(f"Fixture not found: {ROUND1_PSBT}")
+
+    _enroll_treasury(clear_miniscript, offer_minsc_import, press_select)
+
+    start_sign(_load_psbt(ROUND1_PSBT))
+
+    time.sleep(SIMULATOR_DELAY)
+    title, story = cap_story()
+    if title != 'CONTRIBUTE SHARES?':
+        print("\nSTORY BODY:\n", story)
+    assert title == 'CONTRIBUTE SHARES?', f"Unexpected story title: {title!r}"
+    assert 'Silent payment ECDH shares will be added to this transaction' in story, "SP addresses should appear in round 1 story"
+
+    signed = end_sign(accept=True, finalize=False, accept_ms_import=True)
+    tp = BasicPSBT().parse(signed)
+
+    scan_keys = _get_scan_keys(tp)
+    assert len(scan_keys) == NUM_RECIPIENTS, \
+        f"Expected {NUM_RECIPIENTS} SP recipients, got {len(scan_keys)}"
+
+    _assert_round1_output(tp, scan_keys)
+
+def _assert_round1_output(tp, scan_keys):
+    """
+    After Round 1 the PSBT must have, on input[0]:
+    - One sp_partial_ecdh_shares entry per scan key, keyed by (scan_key, our_pk)
+    - One sp_partial_dleq_proofs entry per scan key, keyed by (scan_key, our_pk)
+    - One musig_pubnonces entry (our 0x1b contribution)
+    - No taproot_key_sig (coordinator hasn't aggregated yet)
+    """
+    inp = tp.inputs[0]
+
+    assert inp.sp_partial_ecdh_shares, "Round 1: 0x21 ECDH shares missing on input[0]"
+    assert inp.sp_partial_dleq_proofs, "Round 1: 0x22 DLEQ proofs missing on input[0]"
+    assert inp.musig_pubnonces,   "Round 1: 0x1b pubnonce missing on input[0]"
+    assert not inp.taproot_key_sig, "Round 1: tap_key_sig must not be present"
+
+    participant_key = _our_participant_key(tp)
+
+    # One entry per scan key in the ECDH share map
+    contributed_scan_keys_ecdh = {sk for (sk, pk) in inp.sp_partial_ecdh_shares if pk == participant_key}
+    assert contributed_scan_keys_ecdh == scan_keys, (
+        f"Round 1 ECDH share scan keys mismatch: "
+        f"got {len(contributed_scan_keys_ecdh)}, expected {len(scan_keys)}"
+    )
+
+    # One entry per scan key in the DLEQ proof map
+    contributed_scan_keys_dleq = {sk for (sk, pk) in inp.sp_partial_dleq_proofs if pk == participant_key}
+    assert contributed_scan_keys_dleq == scan_keys, (
+        f"Round 1 DLEQ proof scan keys mismatch: "
+        f"got {len(contributed_scan_keys_dleq)}, expected {len(scan_keys)}"
+    )
+
+    # ECDH share format: compressed point
+    for (sk, pk), share in inp.sp_partial_ecdh_shares.items():
+        assert len(share) == 33 and share[0] in (0x02, 0x03), \
+            f"Invalid ECDH share format for scan_key={sk.hex()}: {share.hex()}"
+
+    # DLEQ proof format: 64 bytes
+    for (sk, pk), proof in inp.sp_partial_dleq_proofs.items():
+        assert len(proof) == 64, \
+            f"DLEQ proof must be 64 bytes for scan_key={sk.hex()}, got {len(proof)}"
+
+    # Exactly one pubnonce from our participant key
+    our_pubnonces = [(pk, ak, lh) for (pk, ak, lh) in inp.musig_pubnonces if pk == participant_key]
+    assert len(our_pubnonces) == 1, \
+        f"Expected 1 pubnonce from our participant key, got {len(our_pubnonces)}"
+    pubnonce = inp.musig_pubnonces[our_pubnonces[0]]
+    assert len(pubnonce) == 66, f"Pubnonce must be 66 bytes, got {len(pubnonce)}"