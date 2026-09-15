Coldcard/firmware

test (MuSig2 + SP): two round integration tests and fixtures
Add cosigner contrib test fixture for simulating round 2 partial signature 
- 1 simulator/signer in test_musig2_sp_full_flow, the other 2 participants 
in 3 of 3 provide contributions from coordinator.

testing/test_musig2_silentpayments.py
@@ -1,15 +1,19 @@
 # (c) Copyright 2026 by Coinkite Inc. This file is covered by license found in COPYING-CC.
 #
-# test_musig2_silentpayments.py - Coldcard simulator tests for MuSig2 + Silent Payments (BIP-327 + BIP-352).
+# test_musig2_silentpayments.py - Coldcard simulator tests for MuSig2 + Silent Payments (BIP-327 + BIP-352)
 #
 # Fixture PSBTs are generated by the Rust binary in bip375-examples:
 #   cargo run -p musig2-signer --bin payroll <path-to-testing/data>
+#
+# Use CC_SP_OUT env var to capture debug artifacts (PSBTs, final transaction) from the test run.
+# ex. export CC_SP_OUT=/tmp/cc-sp-out && pytest test_musig2_silentpayments.py
 
 
 import json
 import os
 import pytest
 import time
+
 from psbt import BasicPSBT
 
 DATA_DIR = os.path.join(os.path.dirname(__file__), 'data')
@@ -17,6 +21,9 @@ ROUND1_PSBT = os.path.join(DATA_DIR, 'musig2-sp-round1-in.psbt')
 # The tr(musig(A,B,C)) treasury descriptor matching the fixtures. Alice (xfp 0f056943) is the
 # simulator's default seed; the device enrolls this wallet before signing.
 TREASURY_DESC = os.path.join(DATA_DIR, 'desc-musig-sp-demo.txt')
+# Bob's + Charlie's static Round-1 contributions (pubnonce 0x1b, ECDH shares 0x21, DLEQ 0x22
+# on input[0]). Generated once by the Rust tool from the same keys as ROUND1_PSBT.
+COSIGNER_CONTRIB_PSBT = os.path.join(DATA_DIR, 'musig2-sp-cosigner-contrib.psbt')
 
 NUM_RECIPIENTS = 5
 
@@ -76,9 +83,10 @@ def test_musig2_sp_round1(dev, start_sign, end_sign, cap_story,
     if title != 'CONTRIBUTE SHARES?':
         print("\nSTORY BODY:\n", story)
     assert title == 'CONTRIBUTE SHARES?', f"Unexpected story title: {title!r}"
-    assert 'Silent payment ECDH shares will be added to this transaction' in story, "SP addresses should appear in round 1 story"
+    assert 'ECDH shares' in story
+    assert 'Other signers' in story
 
-    signed = end_sign(accept=True, finalize=False, accept_ms_import=True)
+    signed = end_sign(accept=True, finalize=False, accept_ms_import=False)
     tp = BasicPSBT().parse(signed)
 
     scan_keys = _get_scan_keys(tp)
@@ -134,3 +142,201 @@ def _assert_round1_output(tp, scan_keys):
         f"Expected 1 pubnonce from our participant key, got {len(our_pubnonces)}"
     pubnonce = inp.musig_pubnonces[our_pubnonces[0]]
     assert len(pubnonce) == 66, f"Pubnonce must be 66 bytes, got {len(pubnonce)}"
+
+# ---------------------------------------------------------------------------
+# Round 1 and partial Round 2 test
+# ---------------------------------------------------------------------------
+
+def test_musig2_sp_full_flow(dev, start_sign, end_sign, cap_story, sim_exec,
+                             clear_miniscript, offer_minsc_import, press_select):
+    """
+    Full two-round flow with the simulator as Alice:
+
+      Round 1 (live)  -> device emits Alice's pubnonce (0x1b), ECDH share (0x21), DLEQ (0x22)
+      Merge           -> add Bob's + Charlie's static Round-1 contributions
+      Round 2 (live)  -> device verifies the cosigners' DLEQ proofs, sums all three ECDH shares,
+                         derives the SP output scripts, aggregates all three nonces, and emits
+                         Alice's partial sig (0x1c)
+
+    Only Alice signs, so the device produces one partial sig and does NOT finalize (it cannot
+    aggregate until all three partials are present).
+    """
+    merged, alice_pk, scan_keys = _alice_round1_and_merge(
+        start_sign, end_sign, cap_story, sim_exec,
+        clear_miniscript, offer_minsc_import, press_select)
+
+    # ---- After the merge, all three participants' Round-1 material must be present ----
+    inp = merged.inputs[0]
+    assert len(inp.musig_pubnonces) == 3, \
+        f"Expected 3 pubnonces after merge, got {len(inp.musig_pubnonces)}"
+    assert len(inp.musig_partial_ecdh_shares) == 3 * NUM_RECIPIENTS, (
+        f"Expected {3 * NUM_RECIPIENTS} ECDH share entries "
+        f"(3 participants × {NUM_RECIPIENTS} scan keys), got {len(inp.musig_partial_ecdh_shares)}"
+    )
+    assert len(inp.musig_partial_dleq_proofs) == 3 * NUM_RECIPIENTS, (
+        f"Expected {3 * NUM_RECIPIENTS} DLEQ proof entries, got {len(inp.musig_partial_dleq_proofs)}"
+    )
+    for sk in scan_keys:
+        ecdh_pks = {pk for (s, pk) in inp.musig_partial_ecdh_shares if s == sk}
+        dleq_pks = {pk for (s, pk) in inp.musig_partial_dleq_proofs if s == sk}
+        assert len(ecdh_pks) == 3, \
+            f"scan_key {sk.hex()}: expected 3 ECDH participants, got {len(ecdh_pks)}"
+        assert ecdh_pks == dleq_pks, \
+            f"scan_key {sk.hex()}: ECDH and DLEQ participant sets differ"
+
+    # ---- Round 2: device signs the merged PSBT ----
+    start_sign(merged.as_bytes())
+    time.sleep(SIMULATOR_DELAY)
+    title, story = cap_story()
+    assert title == 'OK TO SEND?', f"R2 unexpected story title: {title!r}"
+    assert 'sp1' in story, "SP addresses should appear in round 2 confirmation story"
+
+    signed = end_sign(accept=True, finalize=False, accept_ms_import=False)
+    tp = BasicPSBT().parse(signed)
+
+    scan_keys = _get_scan_keys(tp)
+    assert len(scan_keys) == NUM_RECIPIENTS, \
+        f"Expected {NUM_RECIPIENTS} SP recipients, got {len(scan_keys)}"
+
+    _assert_round2_output(tp, scan_keys, alice_pk)
+
+    # Hand off Alice's R2-signed PSBT to the Rust coordinator for finalization.
+    _maybe_write_artifact(signed, "r2-alice.psbt")
+
+def test_musig2_sp_round2_tampered_recipient(dev, start_sign, end_sign, cap_story, sim_exec,
+                                             clear_miniscript, offer_minsc_import, press_select, press_cancel):
+    """
+    Negative test: a coordinator writes a PSBT_OUT_SCRIPT for an SP output that differs from the
+    one derivable from the contributed ECDH shares (swapped recipient). The device must re-derive
+    the script, detect the mismatch, refuse to sign, and emit no partial sig.
+
+    We build the same merged Round-2 PSBT as the happy path, then overwrite one SP output's
+    script with a well-formed but wrong P2TR before submitting for signing. A wrong (not empty)
+    script is required: an empty script is silently filled by the device, so only a present-and-
+    wrong script reaches the mismatch branch in silentpayments.py.
+    """
+    merged, _alice_pk, _scan_keys = _alice_round1_and_merge(
+        start_sign, end_sign, cap_story, sim_exec,
+        clear_miniscript, offer_minsc_import, press_select)
+
+    # Tamper: overwrite one SP output's script with a well-formed but wrong P2TR (OP_1 <32 bytes>).
+    tampered = False
+    for out in merged.outputs:
+        if out.sp_v0_info:
+            out.script = b'\x51\x20' + (b'\xab' * 32)
+            tampered = True
+            break
+    assert tampered, "No SP output found to tamper"
+
+    # Round 2: device must detect the script mismatch and refuse to produce a partial sig.
+    start_sign(merged.as_bytes())
+    time.sleep(SIMULATOR_DELAY)
+    title, story = cap_story()
+
+    if title == 'OK TO SEND?':
+        # Approval shown: the mismatch should fire during signing, so end_sign must raise.
+        with pytest.raises(Exception):
+            end_sign(accept=True, finalize=False, accept_ms_import=False)
+    else:
+        # Mismatch detected during PSBT validation; an error story is shown.
+        assert 'recipient' in story.lower() or 'mismatch' in story.lower() or \
+               'invalid' in story.lower() or 'fail' in story.lower(), \
+            f"Expected tamper-detection error, got title={title!r} story={story!r}"
+
+    press_cancel()
+
+def _alice_round1_and_merge(start_sign, end_sign, cap_story, sim_exec,
+                            clear_miniscript, offer_minsc_import, press_select):
+    """
+    Run Alice's live Round 1 against ROUND1_PSBT, then merge Bob's + Charlie's static Round-1
+    contributions. Returns (merged_psbt, alice_pk, scan_keys).
+    """
+    if not (os.path.exists(ROUND1_PSBT) and os.path.exists(COSIGNER_CONTRIB_PSBT)):
+        pytest.skip(f"Test PSBT fixtures not found at {DATA_DIR}")
+
+    # Clears MUSIG_SESSION_CACHE first: the session-scoped simulator + module-global cache would
+    # otherwise let a prior test would interfere with Alice's R1->R2 nonce regeneration.
+    sim_exec("from psbt import MUSIG_SESSION_CACHE; MUSIG_SESSION_CACHE.clear()")
+
+    _enroll_treasury(clear_miniscript, offer_minsc_import, press_select)
+
+    # ---- Round 1: Alice (the simulator) contributes nonce + ECDH share + DLEQ ----
+    start_sign(_load_psbt(ROUND1_PSBT))
+    time.sleep(SIMULATOR_DELAY)
+    title, _ = cap_story()
+    assert title == 'CONTRIBUTE SHARES?', f"R1 unexpected story title: {title!r}"
+    r1_out = BasicPSBT().parse(end_sign(accept=True, finalize=False, accept_ms_import=False))
+
+    alice_pk = _our_participant_key(r1_out)
+
+    # ---- Merge Bob's + Charlie's static Round-1 contributions ----
+    contrib = BasicPSBT().parse(_load_psbt(COSIGNER_CONTRIB_PSBT))
+    _merge_cosigner_contrib(r1_out, contrib)
+
+    scan_keys = _get_scan_keys(r1_out)
+    assert len(scan_keys) == NUM_RECIPIENTS, \
+        f"Expected {NUM_RECIPIENTS} SP recipients, got {len(scan_keys)}"
+
+    return r1_out, alice_pk, scan_keys
+
+def _assert_round2_output(tp, scan_keys, participant_key):
+    """
+    After Round 2 the PSBT must have, on input[0]:
+    - Exactly one musig_part_sigs entry, from our participant key (0x1c). Only Alice signs here;
+      the device cannot finalize until all three partials are present (done later in Rust).
+    - No tap_key_sig yet.
+
+    And every SP output must have a derived P2TR script.
+    """
+    inp = tp.inputs[0]
+
+    assert inp.musig_part_sigs, "Round 2: 0x1c partial sig missing on input[0]"
+
+    our_part_sigs = [(pk, ak, lh) for (pk, ak, lh) in inp.musig_part_sigs if pk == participant_key]
+    assert len(our_part_sigs) == 1, \
+        f"Round 2: expected exactly 1 partial sig from our participant, got {len(our_part_sigs)}"
+    assert len(inp.musig_part_sigs) == 1, \
+        f"Round 2: only Alice should have signed, got {len(inp.musig_part_sigs)} partial sigs"
+
+    part_sig = inp.musig_part_sigs[our_part_sigs[0]]
+    assert len(part_sig) == 32, \
+        f"MuSig2 partial sig must be 32 bytes, got {len(part_sig)}"
+
+    # Every SP output must have a finalized P2TR script
+    sp_scripts = []
+    for out in tp.outputs:
+        if out.sp_v0_info:
+            assert out.script is not None, "SP output script not set after round 2"
+            assert len(out.script) == 34, \
+                f"SP P2TR script must be 34 bytes (OP_1 <32-byte xonly>), got {len(out.script)}"
+            assert out.script[0] == 0x51, \
+                f"SP output must start with OP_1 (0x51), got 0x{out.script[0]:02x}"
+            sp_scripts.append(bytes(out.script))
+
+    assert len(sp_scripts) == NUM_RECIPIENTS, \
+        f"Expected {NUM_RECIPIENTS} SP output scripts, got {len(sp_scripts)}"
+
+    return sp_scripts
+
+def _maybe_write_artifact(data, filename):
+    """If CC_SP_OUT is set, write a demo artifact into that directory."""
+    out_dir = os.environ.get('CC_SP_OUT')
+    if not out_dir:
+        return
+    os.makedirs(out_dir, exist_ok=True)
+    path = os.path.join(out_dir, filename)
+    with open(path, 'wb') as f:
+        f.write(data)
+    print(f"Wrote {filename} to {path}")
+
+def _merge_cosigner_contrib(target_tp, contrib_tp):
+    """
+    Merge Bob's + Charlie's static Round-1 contributions into input[0] of a PSBT that already
+    holds Alice's live Round-1 output. Copies the pubnonce (0x1b), partial ECDH share (0x21), and
+    DLEQ proof (0x22) maps.
+    """
+    src = contrib_tp.inputs[0]
+    dst = target_tp.inputs[0]
+    dst.musig_pubnonces.update(src.musig_pubnonces)
+    dst.musig_partial_ecdh_shares.update(src.musig_partial_ecdh_shares)
+    dst.musig_partial_dleq_proofs.update(src.musig_partial_dleq_proofs)

macgyver13/coldcard-firmware

testing/test_musig2_silentpayments.py
@@ -1,15 +1,19 @@
 # (c) Copyright 2026 by Coinkite Inc. This file is covered by license found in COPYING-CC.
 #
-# test_musig2_silentpayments.py - Coldcard simulator tests for MuSig2 + Silent Payments (BIP-327 + BIP-352).
+# test_musig2_silentpayments.py - Coldcard simulator tests for MuSig2 + Silent Payments (BIP-327 + BIP-352)
 #
 # Fixture PSBTs are generated by the Rust binary in bip375-examples:
 #   cargo run -p musig2-signer --bin payroll <path-to-testing/data>
+#
+# Use CC_SP_OUT env var to capture debug artifacts (PSBTs, final transaction) from the test run.
+# ex. export CC_SP_OUT=/tmp/cc-sp-out && pytest test_musig2_silentpayments.py
 
 
 import json
 import os
 import pytest
 import time
+
 from psbt import BasicPSBT
 
 DATA_DIR = os.path.join(os.path.dirname(__file__), 'data')
@@ -17,6 +21,9 @@ ROUND1_PSBT = os.path.join(DATA_DIR, 'musig2-sp-round1-in.psbt')
 # The tr(musig(A,B,C)) treasury descriptor matching the fixtures. Alice (xfp 0f056943) is the
 # simulator's default seed; the device enrolls this wallet before signing.
 TREASURY_DESC = os.path.join(DATA_DIR, 'desc-musig-sp-demo.txt')
+# Bob's + Charlie's static Round-1 contributions (pubnonce 0x1b, ECDH shares 0x21, DLEQ 0x22
+# on input[0]). Generated once by the Rust tool from the same keys as ROUND1_PSBT.
+COSIGNER_CONTRIB_PSBT = os.path.join(DATA_DIR, 'musig2-sp-cosigner-contrib.psbt')
 
 NUM_RECIPIENTS = 5
 
@@ -76,9 +83,10 @@ def test_musig2_sp_round1(dev, start_sign, end_sign, cap_story,
     if title != 'CONTRIBUTE SHARES?':
         print("\nSTORY BODY:\n", story)
     assert title == 'CONTRIBUTE SHARES?', f"Unexpected story title: {title!r}"
-    assert 'Silent payment ECDH shares will be added to this transaction' in story, "SP addresses should appear in round 1 story"
+    assert 'ECDH shares' in story
+    assert 'Other signers' in story
 
-    signed = end_sign(accept=True, finalize=False, accept_ms_import=True)
+    signed = end_sign(accept=True, finalize=False, accept_ms_import=False)
     tp = BasicPSBT().parse(signed)
 
     scan_keys = _get_scan_keys(tp)
@@ -134,3 +142,201 @@ def _assert_round1_output(tp, scan_keys):
         f"Expected 1 pubnonce from our participant key, got {len(our_pubnonces)}"
     pubnonce = inp.musig_pubnonces[our_pubnonces[0]]
     assert len(pubnonce) == 66, f"Pubnonce must be 66 bytes, got {len(pubnonce)}"
+
+# ---------------------------------------------------------------------------
+# Round 1 and partial Round 2 test
+# ---------------------------------------------------------------------------
+
+def test_musig2_sp_full_flow(dev, start_sign, end_sign, cap_story, sim_exec,
+                             clear_miniscript, offer_minsc_import, press_select):
+    """
+    Full two-round flow with the simulator as Alice:
+
+      Round 1 (live)  -> device emits Alice's pubnonce (0x1b), ECDH share (0x21), DLEQ (0x22)
+      Merge           -> add Bob's + Charlie's static Round-1 contributions
+      Round 2 (live)  -> device verifies the cosigners' DLEQ proofs, sums all three ECDH shares,
+                         derives the SP output scripts, aggregates all three nonces, and emits
+                         Alice's partial sig (0x1c)
+
+    Only Alice signs, so the device produces one partial sig and does NOT finalize (it cannot
+    aggregate until all three partials are present).
+    """
+    merged, alice_pk, scan_keys = _alice_round1_and_merge(
+        start_sign, end_sign, cap_story, sim_exec,
+        clear_miniscript, offer_minsc_import, press_select)
+
+    # ---- After the merge, all three participants' Round-1 material must be present ----
+    inp = merged.inputs[0]
+    assert len(inp.musig_pubnonces) == 3, \
+        f"Expected 3 pubnonces after merge, got {len(inp.musig_pubnonces)}"
+    assert len(inp.sp_partial_ecdh_shares) == 3 * NUM_RECIPIENTS, (
+        f"Expected {3 * NUM_RECIPIENTS} ECDH share entries "
+        f"(3 participants × {NUM_RECIPIENTS} scan keys), got {len(inp.sp_partial_ecdh_shares)}"
+    )
+    assert len(inp.sp_partial_dleq_proofs) == 3 * NUM_RECIPIENTS, (
+        f"Expected {3 * NUM_RECIPIENTS} DLEQ proof entries, got {len(inp.sp_partial_dleq_proofs)}"
+    )
+    for sk in scan_keys:
+        ecdh_pks = {pk for (s, pk) in inp.sp_partial_ecdh_shares if s == sk}
+        dleq_pks = {pk for (s, pk) in inp.sp_partial_dleq_proofs if s == sk}
+        assert len(ecdh_pks) == 3, \
+            f"scan_key {sk.hex()}: expected 3 ECDH participants, got {len(ecdh_pks)}"
+        assert ecdh_pks == dleq_pks, \
+            f"scan_key {sk.hex()}: ECDH and DLEQ participant sets differ"
+
+    # ---- Round 2: device signs the merged PSBT ----
+    start_sign(merged.as_bytes())
+    time.sleep(SIMULATOR_DELAY)
+    title, story = cap_story()
+    assert title == 'OK TO SEND?', f"R2 unexpected story title: {title!r}"
+    assert 'sp1' in story, "SP addresses should appear in round 2 confirmation story"
+
+    signed = end_sign(accept=True, finalize=False, accept_ms_import=False)
+    tp = BasicPSBT().parse(signed)
+
+    scan_keys = _get_scan_keys(tp)
+    assert len(scan_keys) == NUM_RECIPIENTS, \
+        f"Expected {NUM_RECIPIENTS} SP recipients, got {len(scan_keys)}"
+
+    _assert_round2_output(tp, scan_keys, alice_pk)
+
+    # Hand off Alice's R2-signed PSBT to the Rust coordinator for finalization.
+    _maybe_write_artifact(signed, "r2-alice.psbt")
+
+def test_musig2_sp_round2_tampered_recipient(dev, start_sign, end_sign, cap_story, sim_exec,
+                                             clear_miniscript, offer_minsc_import, press_select, press_cancel):
+    """
+    Negative test: a coordinator writes a PSBT_OUT_SCRIPT for an SP output that differs from the
+    one derivable from the contributed ECDH shares (swapped recipient). The device must re-derive
+    the script, detect the mismatch, refuse to sign, and emit no partial sig.
+
+    We build the same merged Round-2 PSBT as the happy path, then overwrite one SP output's
+    script with a well-formed but wrong P2TR before submitting for signing. A wrong (not empty)
+    script is required: an empty script is silently filled by the device, so only a present-and-
+    wrong script reaches the mismatch branch in silentpayments.py.
+    """
+    merged, _alice_pk, _scan_keys = _alice_round1_and_merge(
+        start_sign, end_sign, cap_story, sim_exec,
+        clear_miniscript, offer_minsc_import, press_select)
+
+    # Tamper: overwrite one SP output's script with a well-formed but wrong P2TR (OP_1 <32 bytes>).
+    tampered = False
+    for out in merged.outputs:
+        if out.sp_v0_info:
+            out.script = b'\x51\x20' + (b'\xab' * 32)
+            tampered = True
+            break
+    assert tampered, "No SP output found to tamper"
+
+    # Round 2: device must detect the script mismatch and refuse to produce a partial sig.
+    start_sign(merged.as_bytes())
+    time.sleep(SIMULATOR_DELAY)
+    title, story = cap_story()
+
+    if title == 'OK TO SEND?':
+        # Approval shown: the mismatch should fire during signing, so end_sign must raise.
+        with pytest.raises(Exception):
+            end_sign(accept=True, finalize=False, accept_ms_import=False)
+    else:
+        # Mismatch detected during PSBT validation; an error story is shown.
+        assert 'recipient' in story.lower() or 'mismatch' in story.lower() or \
+               'invalid' in story.lower() or 'fail' in story.lower(), \
+            f"Expected tamper-detection error, got title={title!r} story={story!r}"
+
+    press_cancel()
+
+def _alice_round1_and_merge(start_sign, end_sign, cap_story, sim_exec,
+                            clear_miniscript, offer_minsc_import, press_select):
+    """
+    Run Alice's live Round 1 against ROUND1_PSBT, then merge Bob's + Charlie's static Round-1
+    contributions. Returns (merged_psbt, alice_pk, scan_keys).
+    """
+    if not (os.path.exists(ROUND1_PSBT) and os.path.exists(COSIGNER_CONTRIB_PSBT)):
+        pytest.skip(f"Test PSBT fixtures not found at {DATA_DIR}")
+
+    # Clears MUSIG_SESSION_CACHE first: the session-scoped simulator + module-global cache would
+    # otherwise let a prior test would interfere with Alice's R1->R2 nonce regeneration.
+    sim_exec("from psbt import MUSIG_SESSION_CACHE; MUSIG_SESSION_CACHE.clear()")
+
+    _enroll_treasury(clear_miniscript, offer_minsc_import, press_select)
+
+    # ---- Round 1: Alice (the simulator) contributes nonce + ECDH share + DLEQ ----
+    start_sign(_load_psbt(ROUND1_PSBT))
+    time.sleep(SIMULATOR_DELAY)
+    title, _ = cap_story()
+    assert title == 'CONTRIBUTE SHARES?', f"R1 unexpected story title: {title!r}"
+    r1_out = BasicPSBT().parse(end_sign(accept=True, finalize=False, accept_ms_import=False))
+
+    alice_pk = _our_participant_key(r1_out)
+
+    # ---- Merge Bob's + Charlie's static Round-1 contributions ----
+    contrib = BasicPSBT().parse(_load_psbt(COSIGNER_CONTRIB_PSBT))
+    _merge_cosigner_contrib(r1_out, contrib)
+
+    scan_keys = _get_scan_keys(r1_out)
+    assert len(scan_keys) == NUM_RECIPIENTS, \
+        f"Expected {NUM_RECIPIENTS} SP recipients, got {len(scan_keys)}"
+
+    return r1_out, alice_pk, scan_keys
+
+def _assert_round2_output(tp, scan_keys, participant_key):
+    """
+    After Round 2 the PSBT must have, on input[0]:
+    - Exactly one musig_part_sigs entry, from our participant key (0x1c). Only Alice signs here;
+      the device cannot finalize until all three partials are present (done later in Rust).
+    - No tap_key_sig yet.
+
+    And every SP output must have a derived P2TR script.
+    """
+    inp = tp.inputs[0]
+
+    assert inp.musig_part_sigs, "Round 2: 0x1c partial sig missing on input[0]"
+
+    our_part_sigs = [(pk, ak, lh) for (pk, ak, lh) in inp.musig_part_sigs if pk == participant_key]
+    assert len(our_part_sigs) == 1, \
+        f"Round 2: expected exactly 1 partial sig from our participant, got {len(our_part_sigs)}"
+    assert len(inp.musig_part_sigs) == 1, \
+        f"Round 2: only Alice should have signed, got {len(inp.musig_part_sigs)} partial sigs"
+
+    part_sig = inp.musig_part_sigs[our_part_sigs[0]]
+    assert len(part_sig) == 32, \
+        f"MuSig2 partial sig must be 32 bytes, got {len(part_sig)}"
+
+    # Every SP output must have a finalized P2TR script
+    sp_scripts = []
+    for out in tp.outputs:
+        if out.sp_v0_info:
+            assert out.script is not None, "SP output script not set after round 2"
+            assert len(out.script) == 34, \
+                f"SP P2TR script must be 34 bytes (OP_1 <32-byte xonly>), got {len(out.script)}"
+            assert out.script[0] == 0x51, \
+                f"SP output must start with OP_1 (0x51), got 0x{out.script[0]:02x}"
+            sp_scripts.append(bytes(out.script))
+
+    assert len(sp_scripts) == NUM_RECIPIENTS, \
+        f"Expected {NUM_RECIPIENTS} SP output scripts, got {len(sp_scripts)}"
+
+    return sp_scripts
+
+def _maybe_write_artifact(data, filename):
+    """If CC_SP_OUT is set, write a demo artifact into that directory."""
+    out_dir = os.environ.get('CC_SP_OUT')
+    if not out_dir:
+        return
+    os.makedirs(out_dir, exist_ok=True)
+    path = os.path.join(out_dir, filename)
+    with open(path, 'wb') as f:
+        f.write(data)
+    print(f"Wrote {filename} to {path}")
+
+def _merge_cosigner_contrib(target_tp, contrib_tp):
+    """
+    Merge Bob's + Charlie's static Round-1 contributions into input[0] of a PSBT that already
+    holds Alice's live Round-1 output. Copies the pubnonce (0x1b), partial ECDH share (0x21), and
+    DLEQ proof (0x22) maps.
+    """
+    src = contrib_tp.inputs[0]
+    dst = target_tp.inputs[0]
+    dst.musig_pubnonces.update(src.musig_pubnonces)
+    dst.sp_partial_ecdh_shares.update(src.sp_partial_ecdh_shares)
+    dst.sp_partial_dleq_proofs.update(src.sp_partial_dleq_proofs)